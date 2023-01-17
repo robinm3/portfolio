23b0e89297d4fe3de2562d1a14957ff9321fb753
marc-antoine robin <marc-antoine.robin.1@ulaval.ca>
2023-01-17 15:29:27 -0500

    fix

--- src/App.tsx
+++ src/App.tsx
@@ -12,14 +12,8 @@ import {
   Grommet,
   Header,
   Heading,
-  Meter,
-  Page,
-  PageContent,
-  PageHeader,
   Paragraph,
-  Stack,
   Text,
-  Markdown,
   Tip,
 } from "grommet";
 import { Moon, Sun } from "grommet-icons";
@@ -118,49 +112,46 @@ function App() {
         className="moving"
         src="https://cdn-icons-png.flaticon.com/512/870/870194.png"
       ></Image>
-      <Page>
-        <AppBar>
-          <Text size="large">Marc-Antoine Robin</Text>
-          <Button
-            a11yTitle={dark ? "light mode" : "dark mode"}
-            icon={dark ? <Moon /> : <Sun />}
-            onClick={() => setDark(!dark)}
+      <AppBar>
+        <Text size="large">Marc-Antoine Robin</Text>
+        <Button
+          a11yTitle={dark ? "light mode" : "dark mode"}
+          icon={dark ? <Moon /> : <Sun />}
+          onClick={() => setDark(!dark)}
+        />
+      </AppBar>
+      <Box margin="small"></Box>
+      <CardTemplate>
+        <Box direction="row" justify="evenly" style={flexwrap}>
+          <LinkTemplate href={cv} name={"CV"} tip={"Curriculum Vitae"} />
+          <LinkTemplate
+            href={"https://github.com/robinm3"}
+            tip="Github"
+            linkToLogo="https://github.githubassets.com/images/modules/logos_page/GitHub-Mark.png"
           />
-        </AppBar>
-        <PageContent>
-          <Box margin="small"></Box>
-          <CardTemplate>
-            <Box direction="row" justify="evenly" style={flexwrap}>
-              <LinkTemplate href={cv} name={"CV"} tip={"Curriculum Vitae"} />
-              <LinkTemplate
-                href={"https://github.com/robinm3"}
-                tip="Github"
-                linkToLogo="https://github.githubassets.com/images/modules/logos_page/GitHub-Mark.png"
-              />
-              <LinkTemplate
-                href={"https://www.linkedin.com/in/marc-antoine-robin"}
-                tip="Linkedin"
-                linkToLogo="https://external-content.duckduckgo.com/iu/?u=http%3A%2F%2F1000logos.net%2Fwp-content%2Fuploads%2F2017%2F03%2FColor-of-the-LinkedIn-Logo.jpg&f=1&nofb=1&ipt=3574f9f820310ae8c775b941ba57b718f37cf2754bfd6719684f04e810479a88&ipo=images"
-              />
-            </Box>
-          </CardTemplate>
-          <CardTemplate>
-            <Box direction="row-responsive" justify="evenly">
-              <Image
-                src="https://aeglo.ift.ulaval.ca/executives/finissant.jpeg"
-                margin="small"
-                className="image"
-              />
-              <Box width="medium" direction="column" justify="center">
-                <Paragraph size="xxlarge" margin="small">
-                  Finissant en génie logiciel, développeur fullstack et artiste
-                  amateur.
-                </Paragraph>
-              </Box>
-            </Box>
-          </CardTemplate>
-        </PageContent>
-      </Page>
+          <LinkTemplate
+            href={"https://www.linkedin.com/in/marc-antoine-robin"}
+            tip="Linkedin"
+            linkToLogo="https://external-content.duckduckgo.com/iu/?u=http%3A%2F%2F1000logos.net%2Fwp-content%2Fuploads%2F2017%2F03%2FColor-of-the-LinkedIn-Logo.jpg&f=1&nofb=1&ipt=3574f9f820310ae8c775b941ba57b718f37cf2754bfd6719684f04e810479a88&ipo=images"
+          />
+        </Box>
+      </CardTemplate>
+      <CardTemplate>
+        <Box direction="row-responsive" justify="evenly">
+          <Image
+            src="https://aeglo.ift.ulaval.ca/executives/finissant.jpeg"
+            margin="small"
+            className="image"
+          />
+          <Box justify="center">
+            <Paragraph size="xxlarge" margin="small">
+              Finissant en génie logiciel, développeur fullstack et artiste
+              amateur.
+            </Paragraph>
+          </Box>
+        </Box>
+      </CardTemplate>
+      <div className="block"></div>
     </Grommet>
   );
 }
